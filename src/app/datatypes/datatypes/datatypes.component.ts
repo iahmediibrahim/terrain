803485b34dbf7df15e9837b921f66b0c01eebe37
swethusiast <aitc.dev@gmail.com>
2019-10-15 11:59:43 +0200

    data types design and service

--- src/app/datatypes/datatypes/datatypes.component.ts
+++ src/app/datatypes/datatypes/datatypes.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { DatatypesService } from '../datatypes-service/datatypes.service';
 
 @Component({
   selector: 'app-datatypes',
   templateUrl: './datatypes.component.html',
-  styleUrls: ['./datatypes.component.scss']
+  styleUrls: ['./datatypes.component.scss'],
 })
 export class DatatypesComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private dataTypes: DatatypesService) {}
+  datatypes;
   ngOnInit() {
+    this.dataTypes.all().subscribe(datatypes => (this.datatypes = datatypes));
   }
-
 }
