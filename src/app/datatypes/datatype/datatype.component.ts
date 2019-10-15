1d923440ab40ffea7ea6bb050b1983abe3e1a9fe
swethusiast <aitc.dev@gmail.com>
2019-10-15 12:51:15 +0200

    datatype table design

--- src/app/datatypes/datatype/datatype.component.ts
+++ src/app/datatypes/datatype/datatype.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatatypesService } from '../datatypes-service/datatypes.service';
 
 @Component({
   selector: 'app-datatype',
   templateUrl: './datatype.component.html',
-  styleUrls: ['./datatype.component.scss']
+  styleUrls: ['./datatype.component.scss'],
 })
 export class DatatypeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private datatypesService: DatatypesService,
+  ) {}
+  datatype;
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.getProject(id);
   }
-
+  getProject(id) {
+    this.datatypesService.getProjectWithId(id).subscribe(datatype => {
+      this.datatype = datatype;
+    });
+  }
 }
